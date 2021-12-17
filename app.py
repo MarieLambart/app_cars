3147cf083d034db5a7c6f9fff6f3f95b34845cca
Marie Lambart <90702580+MarieLambart@users.noreply.github.com>
2021-12-17 17:18:25 +0100

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,9 @@ try :
   
   else :
     
-    st.write("Correlation Heatmap for country(ies) that you choose.")
+    col1= st.columns(1)
+    
+    col1.write("Correlation Heatmap for country(ies) that you choose.")
     
     data = df.loc[countries]
     viz_correlation = sns.heatmap(data.corr(),center=0,cmap= sns.diverging_palette(220, 10,  as_cmap=True), vmax=1, vmin=-1)
