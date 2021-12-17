639b31084d438a5726f573cc4d1bede8387974bf
Marie Lambart <90702580+MarieLambart@users.noreply.github.com>
2021-12-17 11:54:04 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-dict = {"US":"Us.","Europe":"Europe.","Japan":"Japan."}
 
-st.selectbox(
-     'How would you like to be choosed?',
-     (dict.keys()))
+
+st.selectbox('How would you like to be choosed?',("Us","Europe","Japan"))
