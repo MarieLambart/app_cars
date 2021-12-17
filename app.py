24a9c590dafc883a265387fe456e18060084f142
Marie Lambart <90702580+MarieLambart@users.noreply.github.com>
2021-12-17 11:59:02 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 options = st.multiselect('Choisissez une ou plusieurs région(s)?',("US","Europe","Japan"))
 
-st.write('Vous avez sélectionné:', options)
+st.write('Vous avez sélectionné:', str(options))
