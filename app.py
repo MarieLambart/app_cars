137f36be5c9c007ba67da9a868582cd0fba8861b
Marie Lambart <90702580+MarieLambart@users.noreply.github.com>
2021-12-17 16:24:31 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+from urllib.error import URLError
 
 @st.cache
 def get_data ():
@@ -24,3 +25,14 @@ try :
     data = df.loc[countries]
     viz_correlation = sns.heatmap(data.corr(),center=0,cmap= sns.color_palette("vlog", as_cmap=True))
     st.pyplot(viz_correlation.figure)
+
+except URLError as e:
+  
+    st.error(
+        """
+        **This demo requires internet access.**
+
+        Connection error: %s
+    """
+        % e.reason
+    )
