fb2a023043d996c1cd6464d70a6e65e85c5a6497
Marie Lambart <90702580+MarieLambart@users.noreply.github.com>
2021-12-21 12:10:52 +0100

    Update app.py

--- app.py
+++ app.py
@@ -57,7 +57,6 @@ try :
     st.write("We can see that there is a strong correlation positive between :\
     Cylinders and Weightlbs, Hp, Cubicinches, Cubicinches and Weightlbs, Hp and Hp and Weightlbs.\
     That's to say if one increases, the other also increases.")
-    st.write("")
     st.write("And a strong correlation negative bewteen : \
     Mpg and Weightlbs, Hp, Cubicinches, Cylinders and Hp and Time-to-60.\
     That's to say if one increases, the other decreases.")
