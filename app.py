c73d0bd806e9cde11de43099c3ddb1c772da5ce9
Marie Lambart <90702580+MarieLambart@users.noreply.github.com>
2021-12-21 15:14:45 +0100

    Update app.py

--- app.py
+++ app.py
@@ -45,6 +45,10 @@ try :
     c2.write("###### Correlation Heatmap of car's dataset for country(ies) that you choose.")
     viz_correlation = sns.heatmap(data.corr(),center=0,cmap= sns.color_palette("coolwarm", as_cmap=True), vmax=1, vmin=-1)
     c2.pyplot(viz_correlation.figure)
+    
+    c4.write("###### Correlation Heatmap of car's dataset for country(ies) that you choose.")
+    viz_correlation_coeff = sns.heatmap(data.corr(),center=0,cmap= sns.color_palette("coolwarm", as_cmap=True), vmax=1, vmin=-1,annot=True)
+    c4.pyplot(viz_correlation_coeff.figure)
    
     
     c1,c2,c3 = st.columns((1,9,1))
