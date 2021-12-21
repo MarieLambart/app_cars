fac02b9bd2cb9d47ccc112eae735afa8e3bcb926
Marie Lambart <90702580+MarieLambart@users.noreply.github.com>
2021-12-21 15:13:28 +0100

    Update app.py

--- app.py
+++ app.py
@@ -94,7 +94,11 @@ try :
     
     c3.write("###### Year distribution of cars.")
     displot_year = sns.displot(data1, x="year",color="dodgerblue")
-    c3.pyplot(displot_year.figure)    
+    c3.pyplot(displot_year.figure) 
+    
+    c4.write("###### Correlation Heatmap of car's dataset for country(ies) that you choose.")
+    viz_correlatio_coeff = sns.heatmap(data.corr(),center=0,cmap= sns.color_palette("coolwarm", as_cmap=True), vmax=1, vmin=-1,annot=True)
+    c4.pyplot(viz_correlation_coeff.figure)
 
 except URLError as e:
   
